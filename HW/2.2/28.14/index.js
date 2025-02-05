209900d36e2c6f2435bc1e21c0d455ec2c64f9f3
Nerya <serinerya@gmail.com>
2025-02-05 11:55:37 +0200

    fixHW

--- HW/2.2/28.14/index.js
+++ HW/2.2/28.14/index.js
@@ -1,16 +1,23 @@
+console.log("script start");
+function isNumNaN(num){
+    if(isNaN(num)){  
+        throw new Error("Invalid input");
+    }    
+}
+
+function reverseNum(num){
+    for(let i = 1; i<= numLength; i++){
+        temp = num % 10;
+        newNum = newNum + (temp * Math.pow(10,(numLength - i)));
+        num = Math.floor(num/10);
+    }    
+}
+
 let num = +prompt("Enter a number");
 let newNum = 0;
 let temp = 0;
 let numLength = num.toString().length;
-
-if(isNaN(num)){  
-    console.error("Invalid input");
-    process.exit();
-}
-for(let i = 1; i<= numLength; i++){
-    temp = num % 10;
-    newNum = newNum + (temp * Math.pow(10,(numLength - i)));
-    num = Math.floor(num/10);
-}
+isNumNaN(num);
+reverseNum(num);
 console.log(newNum);
-console.log("script end")
+console.log("script end");
--- HW/2.2/29.26/index.js
+++ HW/2.2/29.26/index.js
@@ -1,12 +1,24 @@
 console.log("script start");
+function isNumNaN(num){
+
+    if(isNaN(num)){
+        throw new Error("Invalid input");
+    }
+
+}
+
+function showNumbers(num1, num2){
+    for(let i = num1; i<= num2; i++){
+        console.log(i);
+    }    
+}
+
 let num1 = +prompt("Enter the lowest number");
 let num2 = +prompt("Enter the highest number");
 
-if(isNaN(num1) || isNaN(num2)){
-    console.error("Invalid input");
-    process.exit();
-}
-for(let i = num1; i<= num2; i++){
-    console.log(i);
-}
+isNumNaN(num1);
+isNumNaN(num2);
+
+showNumbers(num1, num2);
+
 console.log("script end")
