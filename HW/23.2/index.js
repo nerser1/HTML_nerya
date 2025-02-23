107270c4bda127b289aeb77f5f15587ebb315989
Nerya <serinerya@gmail.com>
2025-02-23 21:35:16 +0200

    o

---
+++ HW/23.2/index.js
@@ -0,0 +1,17 @@
+function loadCards(array){
+    const content = document.getElementById(content)
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        getCardTemplate(element)
+    }
+}
+
+function getCardTemplate(element){
+const {}
+}
+
+function init(){
+    loadCards(dataMovie);
+}
+
+init();
