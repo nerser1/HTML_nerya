eca3ebc7710460d34ed1a23ea7040ea727cc9556
Nerya <serinerya@gmail.com>
2025-03-03 12:03:56 +0200

    9

--- tirgul/2.3.25/financy/index.js
+++ tirgul/2.3.25/financy/index.js
@@ -1,4 +1,42 @@
+function createTable(array){
+    const table = document.createElement("table");
+    const thead = createThead(array);
+    const tbody = createTbody(array);
+    table.append(thead)
+    table.append(tbody)    
+    console.log(table)
+}
 
+function createThead(array){
+    const element = array[0];
+    const properties = Object.keys(element);
+    const thead = document.createElement("thead");
+    console.log(properties)
+    for (let index = 0; index < properties.length; index++) {
+        const td = document.createElement("td");
+        td.append(properties[index]);
+        thead.append(td);
+    }
+    return thead;
+}
+function createTbody(array){
+    const element = array[0];
+    const properties = Object.keys(element);
+    const tbody = document.createElement("tbody");
+    console.log(properties)
+    for (let index = 0; index < array.length; index++) {        
+        const element = array[index];
+        const tr = document.createElement("tr")
+        for (let index = 0; index < properties.length; index++) {
+            const prop = properties[index];
+            const td = document.createElement("td");
+            td.append(element[prop] || "");
+            tr.append(td);
+        }
+        tbody.append(tr);
+    }
+    return tbody;
+}
 
 function init() {
 const _income = document.getElementById("income");
@@ -7,8 +45,11 @@ const _taxes = document.getElementById("taxes");
 const _comments = document.getElementById("comments");
 const _month = document.getElementById("month");
 const calcBtn = document.getElementById("calcBtn");
+const content = document.getElementById("content");
 
 calcBtn.addEventListener("click", function(){
+    const date = new Date();
+    console.log(date);
     const reveneus = localStorage.getItem("reveneus")
     if(reveneus){
         const reveneusArr = JSON.parse(reveneus)
@@ -31,6 +72,9 @@ calcBtn.addEventListener("click", function(){
         })
         localStorage.setItem("reveneus", JSON.stringify(reveneusArr))
     }
+    const table = createTable(JSON.parse(localStorage.getItem("reveneus")))
+    content.innerHTML = table
+
 })
 }
 
